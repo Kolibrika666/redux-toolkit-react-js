96388d4fb40a71b324eaa17eacd2fab4a13344c3
Kolibrika666 <kolibrika666@gmail.com>
2024-02-05 22:37:30 +0700

    init: invalid action payload

--- redux-api/react-redux-galery/src/store/index.js
+++ redux-api/react-redux-galery/src/store/index.js
@@ -1,16 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./counter/counterSlice";
-import cartSlice from "./cart/cartSlice";
-import authSlice from "./auth/authSlice";
 import likesSlice from "./likes/likesSlice";
 import likesItemSlice from "./likesItem/likesItemSlice";
 import galerySlice from "./galerySlice/galerySlice";
 
 const store = configureStore({
   reducer: {
-    counter: counterSlice,
-    cart: cartSlice,
-    auth: authSlice,
     likes: likesSlice,
     likesItem: likesItemSlice,
     galery: galerySlice,
